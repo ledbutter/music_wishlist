207c38c0b81fbbb80f76ce64eb36c0c09463d4b1
ledbutter <svengrosen@gmail.com>
2014-10-09 03:31:35 +0000

    first stab at something

--- main.go
+++ main.go
@@ -1,12 +1,53 @@
 package main
 
-import "net/http"
+import (
+	//	"net/http"
+	"bufio"
+	"fmt"
+	//	"github.com/ledbutter/newmath"
+	"github.com/ledbutter/musicSource"
+	"os"
+	//	"strings"
+	//	"io/ioutil"
+)
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8080", nil)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("What band are you interested in: ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading stdin")
+	}
+	fmt.Println(text)
+	var crit musicSource.SearchCriteria
+	crit.ArtistName = text[:len(text)-1]
+	mbs := new(musicSource.MusicBrainzSearcher)
+	mbs.Criteria = crit
+	result := mbs.Search()
+	fmt.Println(result)
+	//crit := musicSource.SearchCriteria(text, "")
+	//fmt.Println(crit)
+	//temp := newmath.Sqrt(2)
+	//fmt.Println(temp)
+	/*crit := Criteria(artistName, "")
+	mbs := make(MusicBrainzSearcher)
+	mbs.criteria = crit
+	result = mbs.search()
+	fmt.println(result)*/
+	/*resp, err := http.Get("http://www.google.com")
+	if err != nil {
+		println("Uh Oh")
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	contents := string(body)
+	println(contents)
+	*/
+	//http.HandleFunc("/", hello)
+	//http.ListenAndServe(":8080", nil)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
+/*func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello!"))
-}
+}*/
