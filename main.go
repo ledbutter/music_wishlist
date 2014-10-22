35040aa62f45b69359df0ec955d0f58827e50b30
ledbutter <svengrosen@gmail.com>
2014-10-22 02:48:26 +0000

    get album search working...somewhat

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ func main() {
 	if err != nil {
 		fmt.Println("Error reading stdin")
 	}
-	fmt.Println(text)
 	var crit musicSource.SearchCriteria
 	crit.ArtistName = text[:len(text)-1]
+	fmt.Print("What album: ")
+	text, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading stdin")
+	}
+	crit.AlbumName = text[:len(text)-1]
 	mbs := new(musicSource.MusicBrainzSearcher)
 	mbs.Criteria = crit
 	result := mbs.Search()
