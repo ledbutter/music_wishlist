b9912f82db3f92f6072c7b9629e580a209d93e9b
Sven Grosen <svengrosen@gmail.com>
2014-11-02 21:05:19 -0800

    hurray for functioning redis stuff

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/ledbutter/musicSource"
+	"github.com/ledbutter/musicStorage"
 	"os"
 )
 
 func main() {
+
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("What band are you interested in: ")
 	text, err := reader.ReadString('\n')
@@ -24,6 +26,23 @@ func main() {
 	crit.AlbumName = text[:len(text)-1]
 	mbs := new(musicSource.MusicBrainzSearcher)
 	mbs.Criteria = crit
-	result := mbs.Search()
+	result := musicSource.Album{}
+	result, err = mbs.Search()
+	if err != nil {
+		fmt.Println("Error searching")
+	}
 	fmt.Println(result)
+	//now push it to our storage
+	storageAlbum := musicStorage.SavedAlbum{}
+	storageAlbum.Key = result.Id
+	storageAlbum.Title = result.Title
+	storageAlbum.Artist = result.Artist
+	musicStorage.AddAlbum(storageAlbum)
+	fmt.Println("Saved album")
+
+	allAlbums := musicStorage.ListAlbums()
+	for _, a := range allAlbums {
+		fmt.Println(a)
+	}
+
 }
